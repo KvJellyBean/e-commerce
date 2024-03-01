10341e793383664863c79d364acadefb365c3285
XTu17As <richard91.us@gmail.com>
2024-03-02 03:28:35 +0700

    cart Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from "@angular/router";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { HeaderComponent } from "./header/header.component";
 import { MainpageComponent } from "./mainpage/mainpage.component";
+import { CartComponent } from "./cart/cart.component";
 
 @Component({
-  selector: "app-root",
-  standalone: true,
-  imports: [RouterOutlet, NgbModule, HeaderComponent, MainpageComponent],
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+    selector: "app-root",
+    standalone: true,
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.css"],
+    imports: [RouterOutlet, NgbModule, HeaderComponent, MainpageComponent, CartComponent]
 })
 export class AppComponent {
   title = "E-Commerce";
---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart',
+  standalone: true,
+  imports: [],
+  templateUrl: './cart.component.html',
+  styleUrl: './cart.component.css',
+  
+})
+export class CartComponent {
+  // Define your variable
+  product = {
+    name: 'Product Name',
+    price: 'Product Price',
+    description: 'Product Description',
+    image: 'Product Image URL'
+  };
+
+  removeFromCart() {
+    // Implement the logic to remove an item from the cart
+  }
+
+  checkout() {
+    // Implement the checkout logic
+  }
+}
