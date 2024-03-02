84ef90ec48ca035e26ee6e387f299b8d61d62d74
KvJellyBean <kevinnatan91@gmail.com>
2024-03-02 20:13:12 +0700

    feat(footer): implement footer to the website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { RouterOutlet } from "@angular/router";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { HeaderComponent } from "./header/header.component";
 import { MainpageComponent } from "./mainpage/mainpage.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, NgbModule, HeaderComponent, MainpageComponent],
+  imports: [
+    RouterOutlet,
+    NgbModule,
+    HeaderComponent,
+    MainpageComponent,
+    FooterComponent,
+  ],
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
