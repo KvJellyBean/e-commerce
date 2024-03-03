fede29864b3035ffe06ed47b83cb728e4b5d5d2b
Ariwjy <ariwijaya2608@gmail.com>
2024-03-03 21:27:25 +0700

    feat(login): add login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from "@angular/router";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { HeaderComponent } from "./header/header.component";
 import { MainpageComponent } from "./mainpage/mainpage.component";
+import { LoginComponent } from "./login/login.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, NgbModule, HeaderComponent, MainpageComponent],
+  imports: [RouterOutlet, NgbModule, HeaderComponent, MainpageComponent, LoginComponent],
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
