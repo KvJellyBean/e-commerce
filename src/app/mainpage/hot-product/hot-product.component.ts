658941a18a5f45838bde17856f3256d680191623
KvJellyBean <kevinnatan91@gmail.com>
2024-03-05 19:45:06 +0700

    fix(hotproduct): add some router links

--- src/app/mainpage/hot-product/hot-product.component.ts
+++ src/app/mainpage/hot-product/hot-product.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { CartService } from "../../cart/cart.services";
 import { CommonModule } from "@angular/common";
+import { RouterLink } from "@angular/router";
 
 interface Product {
   name: string;
@@ -12,38 +13,38 @@ interface Product {
 @Component({
   selector: "app-hot-product",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: "./hot-product.component.html",
   styleUrl: "./hot-product.component.css",
 })
 export class HotProductComponent {
   products: Product[] = [
     {
-      name: 'Product 1',
-      price: '18.55',
-      description: 'This is product 1',
-      image: './assets/slides/slide1.png',
+      name: "Product 1",
+      price: "18.55",
+      description: "This is product 1",
+      image: "./assets/slides/slide1.png",
     },
 
     {
-      name: 'Product 2',
-      price: '18.55',
-      description: 'This is product 2',
-      image: './assets/slides/slide2.png',
+      name: "Product 2",
+      price: "18.55",
+      description: "This is product 2",
+      image: "./assets/slides/slide2.png",
     },
 
     {
-      name: 'Product 3',
-      price: '18.55',
-      description: 'This is product 3',
-      image: './assets/slides/slide3.png',
+      name: "Product 3",
+      price: "18.55",
+      description: "This is product 3",
+      image: "./assets/slides/slide3.png",
     },
-
   ];
 
   constructor(private cartService: CartService) {}
 
-  addProductToCart(product: Product) {
+  addProductToCart(product: Product, event: Event) {
+    event.preventDefault();
     this.cartService.addToCart(product);
   }
 }
