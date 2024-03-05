1efd8a015223891a7a20d1e2834a2b6d4c529541
KvJellyBean <kevinnatan91@gmail.com>
2024-03-05 19:28:50 +0700

    fix(header): change router link for the nav links
    This commit tries to change the navigation link to connect it with the declared router such as
    Homepage, cart, product, and login/account

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,20 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: "app-header",
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: "./header.component.html",
   styleUrl: "./header.component.css",
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {
+    const links = document.querySelectorAll<HTMLAnchorElement>("ul li a");
+    links.forEach((link) => {
+      link.addEventListener("click", (e) => e.preventDefault());
+    });
+  }
+}
