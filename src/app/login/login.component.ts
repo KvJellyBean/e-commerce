0b9cfe694435f1ad88bca50d144b0ffc2ad901df
KvJellyBean <kevinnatan91@gmail.com>
2024-03-05 19:39:41 +0700

    fix: remove unused data and format code
    This commit removes some image data that unused, and also try to format the code with Prettier.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,46 +1,48 @@
-import { Component, OnInit} from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit } from "@angular/core";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
-  selector: 'app-login',
+  selector: "app-login",
   standalone: true,
   imports: [NgbModule],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  templateUrl: "./login.component.html",
+  styleUrl: "./login.component.css",
 })
-export class LoginComponent implements OnInit{
-  
+export class LoginComponent implements OnInit {
   ngOnInit(): void {
-    const loginregis = document.querySelector('.loginregis');
-    const loginlink = document.querySelector('.login-link');
-    const registerlink = document.querySelector('.register-link');
-    const btnpopup = document.querySelector('.popup-login');
-    const iconclose = document.querySelector('.close');
-    
-    registerlink?.addEventListener('click', () => {
-      loginregis?.classList.add('active');
+    const loginregis = document.querySelector(".loginregis");
+    const loginlink = document.querySelector(".login-link");
+    const registerlink = document.querySelector(".register-link");
+    const btnpopup = document.querySelector(".popup-login");
+    const iconclose = document.querySelector(".close");
+
+    registerlink?.addEventListener("click", () => {
+      loginregis?.classList.add("active");
     });
 
-    loginlink?.addEventListener('click', () => {
-      loginregis?.classList.remove('active');
+    loginlink?.addEventListener("click", () => {
+      loginregis?.classList.remove("active");
     });
 
-    btnpopup?.addEventListener('click', () => {
-      loginregis?.classList.add('active-popup');
+    btnpopup?.addEventListener("click", () => {
+      loginregis?.classList.add("active-popup");
     });
 
-    iconclose?.addEventListener('click', () => {
-      loginregis?.classList.remove('active-popup');
+    iconclose?.addEventListener("click", () => {
+      loginregis?.classList.remove("active-popup");
     });
   }
 
   validateForm(): boolean {
-    const password = (document.getElementById("password") as HTMLInputElement).value;
-    const confirmPassword = (document.getElementById("cpassword") as HTMLInputElement).value;
+    const password = (document.getElementById("password") as HTMLInputElement)
+      .value;
+    const confirmPassword = (
+      document.getElementById("cpassword") as HTMLInputElement
+    ).value;
 
     if (password !== confirmPassword) {
-        alert("Password and confirm password do not match!");
-        return false;
+      alert("Password and confirm password do not match!");
+      return false;
     }
     return true;
   }
@@ -48,13 +50,13 @@ export class LoginComponent implements OnInit{
   togglePassword(field: string, iconId: string) {
     const input = document.getElementById(field) as HTMLInputElement;
     const icon = document.querySelector(`#${iconId}`) as HTMLElement;
- 
-    if (input.type === 'password') {
-        input.type = 'text';
-        icon.className = 'fa-solid bi-eye'; // Eye open when password is revealed
+
+    if (input.type === "password") {
+      input.type = "text";
+      icon.className = "fa-solid bi-eye";
     } else {
-        input.type = 'password';
-        icon.className = 'fa-solid bi-eye-slash'; // Eye closed when password is hidden
+      input.type = "password";
+      icon.className = "fa-solid bi-eye-slash";
     }
-}
+  }
 }
