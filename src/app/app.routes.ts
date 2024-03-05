9a0dc9536cdd1d09b53593b67546eaaf7dd4d1bc
KvJellyBean <kevinnatan91@gmail.com>
2024-03-05 17:51:26 +0700

    feat(route): add page router
    This commit add route for the website

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { MainpageComponent } from "./mainpage/mainpage.component";
+import { LoginComponent } from "./login/login.component";
+import { CartComponent } from "./cart/cart.component";
+import { ShopComponent } from "./shop/shop.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", component: MainpageComponent },
+  { path: "home", component: MainpageComponent },
+  { path: "shop", component: ShopComponent },
+  { path: "cart", component: CartComponent },
+  { path: "login", component: LoginComponent },
+  { path: "**", component: MainpageComponent },
+];
